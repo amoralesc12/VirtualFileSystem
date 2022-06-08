b4d98262bb9ff458e423e7ba7ecf819a9cfcf970
mergi02 <pmercedes@unitec.edu>
2022-06-08 14:21:56 -0600

    avance

--- VirtualFileSystem/MapaBits.h
+++ VirtualFileSystem/MapaBits.h
@@ -1,41 +1,96 @@
 #pragma once
-class MapaBits 
-{
-	//TODO DEBE SER DINAMICO
-	class MB_Directo
-	{
-		char* puntero;
-		
-		char* toChar();
-		void fromChar(char*);
-	};
+#include "DataFile.h"
+#include "MetaData.h"
 
-	class MB_IndirectoN1
-	{
-		char* puntero;
-
-		char* toChar();
-		void fromChar(char*);
-	};
-
-	class MB_IndirectoN2
-	{
-		char* puntero;
-
-		char* toChar();
-		void fromChar(char*);
-	};
-
-	class MB_IndirectosN3
-	{
-		char* puntero;
-
-		char* toChar();
-		void fromChar(char*);
-	};
-
+class MapaBits{	
+public:	
+	
+	/*TODO DEBE SER DINAMICO*/
 	//TOTAL DE BYTES POR CADA MAPA DE BITS
+
+	class MB_BloqueDirecto {
+		friend class MapaBits;
+		MB_BloqueDirecto();
+		char* bloqueDirecto;
+
+		char* toCharBD();
+		void fromCharBD(char*);
+		int getSizeBD();
+	};
+
+	class MB_Indirecto1
+	{
+
+		MB_Indirecto1();
+		MB_Indirecto1(int);
+		char* indirecto1;
+
+		char* toCharIn1();
+		void fromCharIn1(char*);
+		int getSizeBI1();
+	};
+
+
+	class MB_Indirecto2 {
+	public:
+		MB_Indirecto2();
+		MB_Indirecto2(int);
+		char* indirecto2;
+
+		char* toCharIn2();
+		void fromCharIn2(char*);
+
+		int getSizeBI2();
+	};
+
+
+	class MB_Indirecto3
+	{
+	public:
+		MB_Indirecto3();
+		MB_Indirecto3(int);
+		char* indirecto3;
+
+		char* toCharIn3();
+		void fromCharIn3(char*);
+
+		int getSizeBI3();
+	};
+
+
+
+	MapaBits();
+	MapaBits(int);
+	void leerMapaBits(char, int, int, int, int);
+	
 	char* toChar();
+	char* toCharBD();
+	char* toCharIn1();
+	char* toCharIn2();
+	char* toCharIn3();
+
+	int getSizeBD();
+	int getSizeBI1();
+	int getSizeBI2();
+	int getSizeBI3();
+
 	void fromChar(char*);
+	int getSizeOf();
+	void  imprimirMapa();
+	void crearMapaBits();
+
+	char* ptrsCombinados;
+	char* bloque1, * bloque2, * bloque3, * bloque4;
+
+	DataFile* file;
+	MB_BloqueDirecto* dir;
+	MB_Indirecto1* ind1;
+	MB_Indirecto2* ind2;
+	MB_Indirecto3* ind3;
+
+
+
+	//DataFile
 };
+
 
--- VirtualFileSystem/MetaData.h
+++ VirtualFileSystem/MetaData.h
@@ -1,20 +1,34 @@
 #pragma once
+#include "MapaBits.h"
 class MetaData
 {
 private:
-	char nDisco[20];
+	char nombreDisco[20];
 	char fechaCreacion[8];
-
-	int EntradasDirectorios; // la cantidad
+	int EntradasDirectorios(); // la cantidad
 	 
-	unsigned int tamBloque; //tamaño
+	unsigned int tamanioBloque; //tamaño
 	int cantBD;
 	int cantBI_Nivel1;
 	int cantBI_Nivel2;
 	int cantdBI_Nivel3;
 
+	void guardarMapaBits();
+	int getSizeOf();
+	void read(const char* _nombre);
+	const char* getName();
+
+	void print();
+
 public:
 	MetaData();
+	MetaData(const char*, int);
+	DataFile* file;
+	MetaData* meta;
+	MapaBits* bits;
+	
+	crearDisco(int, const char*);
+
 
 	char* toChar();
 	void fromChar(char*);
