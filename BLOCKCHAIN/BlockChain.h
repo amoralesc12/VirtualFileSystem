4abbca38fd5c7c88894271288597c0f526f2f3b1
amoralesc12 <alejandramorales.2001@unitec.edu>
2022-06-24 16:51:31 -0600

    BLOCKCHAIN terminado la implementacion

---
+++ BLOCKCHAIN/BlockChain.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef _BLOCKCHAIN_H
+#define _BLOCKCHAIN_H
+
+#include <cstdint>
+#include <vector>
+#include "Block.h"
+
+using namespace std;
+
+class Blockchain {
+public:
+    Blockchain();
+
+    void AddBlock(Block bNew);
+
+private:
+    uint32_t _nDifficulty;
+    vector<Block> _vChain;
+
+    Block _GetLastBlock() const;
+};
+
+#endif //_BLOCKCHAIN_H
+
